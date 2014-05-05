ba275111b84a24ff937a957d4fc5cbbfc9559a13
Luis Fernandes <zipleen@gmail.com>
2014-05-06 00:18:06 +0100

    new android project

--- cordova/platforms/android/CordovaLib/ant-gen/R.java.d
+++ cordova/platforms/android/CordovaLib/ant-gen/R.java.d
@@ -1,1 +1,1 @@
- : /Users/pedro/Documents/git/find-my-friends/cordova/platforms/android/CordovaLib/ant-build/AndroidManifest.xml \
+ : /Users/zipleen/dev/find-my-friends/cordova/platforms/android/CordovaLib/ant-build/AndroidManifest.xml \
