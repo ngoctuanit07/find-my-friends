efc29c27180b1c07ffc277b5b98ff9cb4cc8a890
pedrorfernandes <pedrorfernandes@yahoo.com>
2014-05-12 19:33:37 +0100

    latest android build updated migration table for users

--- app/database/migrations/2014_04_27_175435_create_users_table.php
+++ app/database/migrations/2014_04_27_175435_create_users_table.php
@@ -23,6 +23,8 @@ class CreateUsersTable extends Migration {
             $table->bigInteger('facebook_uid')->nullable();
             $table->string('photo', 500)->nullable();
             $table->timestamp('last_login')->nullable();
+            $table->string('device_type')->nullable();
+            $table->string('device_token')->nullable();
             $table->timestamps();
         });
 	}
